c0d51b28b79221790fa5b5ccc57d3e1d4e303c93
Akere Adetola <adetolaakere1@gmail.com>
2019-11-11 18:15:33 +0100

    user register and login

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const db = require("./data/dbConfig");
+const Users = require("./Users/users");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 11);
+  const newUser = {
+    username: req.body.username,
+    password: hash
+  };
+
+  Users.add(newUser)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check if the provided password is correct
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+const port = process.env.PORT || 7000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
