b92cf5e477f9cf9e05bded837d51552499a0fc9c
Akere Adetola <adetolaakere1@gmail.com>
2019-11-12 21:48:21 +0100

    cookies and logout

--- index.js
+++ index.js
@@ -2,80 +2,42 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
-const db = require("./data/dbConfig");
-const Users = require("./Users/users");
-const restricted = require("./middleware/restricted");
+const authRouter = require("./auth/authRouter");
 
 const server = express();
+
+const sessionConfig = {
+  name: "Adetola",
+  secret: "make it a little long and keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60, // you need it if the cookie is to survive !!
+    secure: false, // with secure, the cookie only gets set when https !!
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require("./data/dbConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.send("It's alive!");
 });
-
-server.post("/api/register", (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 11);
-  const newUser = {
-    username: req.body.username,
-    password: hash
-  };
-
-  Users.add(newUser)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-server.post("/api/login", (req, res) => {
-  let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      // check if the provided password is correct
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-server.get("/api/users", restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-});
-
-// function restricted(req, res, next) {
-//   // let's assume user & password
-//   const { username, password } = req.headers;
-//   Users.findBy({ username })
-//     .first()
-//     .then(user => {
-//       if (user && bcrypt.compareSync(password, user.password)) {
-//         next();
-//       } else {
-//         res.status(401).json({ message: "Invalid Credentials" });
-//       }
-//     })
-//     .catch(error => {
-//       res.status(500).json({ message: error.message });
-//     });
-// }
 
 const port = process.env.PORT || 7000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
--- middleware/restricted.js
+++ middleware/restricted.js
@@ -4,17 +4,9 @@ const bcrypt = require("bcryptjs");
 module.exports = restricted;
 
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: error.message });
-    });
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
 }
